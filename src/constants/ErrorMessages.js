c2101771148b01d8daa1cf26d1f776f53cf0fdbb
Nighttrek <Daniels@dual4t.com>
2023-06-19 16:16:43 -0700

    centralized the errors in the error store

--- src/constants/ErrorMessages.js
+++
@@ -1,4 +0,0 @@
-export const MISSING_CONFIG =
-  'Missing configuration variables. See our docs: https://docs.fun.xyz/how-to-guides/configure-environment'
-export const MISSING_API_KEY =
-  'Missing API key in the configuration variables. See our docs: https://docs.fun.xyz/how-to-guides/configure-environment'
--- src/store/errorStore.ts
+++ src/store/errorStore.ts
@@ -1,18 +1,43 @@
 export interface FunError {
-    code: number
-    message: string
-    err?: Error | unknown
+  code: number // easy access code
+  message: string // description with resolution
+  link?: string // link to documentation if it exists
+  err?: Error | unknown // the error object if needed
 }
 
 export interface ErrorStoreInterface {
-    error: FunError | null
-    errors: FunError[]
-    setFunError: (error: FunError) => void
-    resetFunError: () => void
-    resetFunErrors: () => void
+  error: FunError | null
+  errors: FunError[]
+  setFunError: (error: FunError) => void
+  resetFunError: () => void
+  resetFunErrors: () => void
+}
+
+export const GeneralFunError: FunError = {
+  code: 0,
+  message: 'Error',
 }
 
 export const MissingConfigError: FunError = {
-    code: 1,
-    message: "Missing Fun Environment Config"
+  code: 1,
+  message: 'Missing Fun Environment Config',
+  link: 'https://docs.fun.xyz/how-to-guides/configure-environment',
+}
+
+export const MissingApiKeyError: FunError = {
+  code: 2,
+  message: 'Missing API key in Config',
+  link: 'https://docs.fun.xyz/how-to-guides/configure-environment',
+}
+
+export const MissingActiveSigner: FunError = {
+  code: 3,
+  message: 'No active signer. Activate a connector before calling this function',
+  link: 'https://docs.fun.xyz/how-to-guides/configure-account/auth-types',
+}
+
+export const LegacyAuthIdMultiAccountError: FunError = {
+  code: 3000,
+  message:
+    'One or more authId is already linked to a different FunWallet. Disconnect the account which has already been linked and try again.',
 }
