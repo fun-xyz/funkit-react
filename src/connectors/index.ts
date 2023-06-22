0cf417e7e15f4d6a9f2bee341bcf256ce8f94bd2
Nighttrek <Daniels@dual4t.com>
2023-06-21 17:05:14 -0700

    fixed the issue with the postInstall location

--- src/connectors/index.ts
+++ src/connectors/index.ts
@@ -1,6 +1,5 @@
 import { CoinbaseWallet } from '@web3-react/coinbase-wallet'
 import { MetaMask } from '@web3-react/metamask'
-import { Network } from '@web3-react/network'
 // import type { Web3ReactHooks } from "@web3-react/core";
 import type { Connector } from '@web3-react/types'
 import { WalletConnect as WalletConnectV2 } from '@web3-react/walletconnect-v2'
@@ -16,7 +15,6 @@ export function getName(connector: Connector | MagicAuthConnector) {
   if (connector instanceof MetaMask) return 'MetaMask'
   if (connector instanceof WalletConnectV2) return 'WalletConnect'
   if (connector instanceof CoinbaseWallet) return 'Coinbase Wallet'
-  if (connector instanceof Network) return 'Network'
   if (connector['name']) {
     return connector['name'].charAt(0).toUpperCase() + connector['name'].slice(1)
   }
