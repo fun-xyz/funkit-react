375d9ba0ea6ba7889ff030f8b33b2f797be5a885
aaronchewbani <aaronchewbani@gmail.com>
2023-07-05 19:29:45 -0700

    push

--- src/hooks/UseGetAssets.ts
+++ src/hooks/UseGetAssets.ts
@@ -1,25 +1,35 @@
-import { useCallback } from 'react'
+import { useCallback, useState } from 'react'
+import { shallow } from 'zustand/shallow'
+
+import { GetAssetsError } from '@/store'
 
 import { useFunStoreInterface } from '../store/CreateUseFunStore'
 import { useFun } from './UseFun'
 
 export const useGetAssets = (build: any) => {
-  const { FunWallet, setAssets, assets } = useFun((state: useFunStoreInterface) => ({
-    FunWallet: state.FunWallet,
-    setAssets: state.setAssets,
-    assets: state.assets,
-  }))
+  const { FunWallet, setAssets, assets, setTempError } = useFun(
+    (state: useFunStoreInterface) => ({
+      FunWallet: state.FunWallet,
+      setAssets: state.setAssets,
+      assets: state.assets,
+      setTempError: state.setTempError,
+    }),
+    shallow
+  )
+  const [loading, setLoading] = useState(true)
 
   const getAssets = useCallback(async () => {
-    if (FunWallet) {
-      try {
-        const getAssetResult = await FunWallet.getAssets()
-        setAssets(getAssetResult)
-      } catch (e) {
-        console.log(e, 'Failed to get Assets')
-      }
+    if (!FunWallet) return
+    setLoading(true)
+    try {
+      const getAssetResult = await FunWallet.getAssets()
+      setAssets(getAssetResult)
+      setLoading(false)
+    } catch (e) {
+      setTempError(GetAssetsError)
     }
+    setLoading(false)
   }, [assets, FunWallet])
 
-  return { getAssets }
+  return { getAssets, loading }
 }
