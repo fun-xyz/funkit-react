9d7cbb6afbe163f85092520893fcaf7db1881098
aaronchewbani <aaronchewbani@gmail.com>
2023-07-05 17:45:30 -0700

    getassets

--- src/hooks/UseGetAssets.ts
+++ src/hooks/UseGetAssets.ts
@@ -4,20 +4,22 @@ import { useFunStoreInterface } from '../store/CreateUseFunStore'
 import { useFun } from './UseFun'
 
 export const useGetAssets = (build: any) => {
-  const { FunWallet, index, error, setTempError } = useFun((state: useFunStoreInterface) => ({
+  const { FunWallet, index, error, setTempError, setAssets, assets } = useFun((state: useFunStoreInterface) => ({
     FunWallet: state.FunWallet,
     index: state.index,
     error: state.error,
     setIndex: state.setIndex,
     setTempError: state.setTempError,
+    setAssets: state.setAssets,
+    assets: state.assets,
   }))
 
   const getAssets = useCallback(async () => {
     if (FunWallet) {
-      const assets = await FunWallet.getAssets()
-      return assets
+      const getAssetResult = await FunWallet.getAssets()
+      setAssets(getAssetResult)
     }
-  }, [FunWallet])
+  }, [assets, FunWallet])
 
   return { index, error, setTempError, getAssets }
 }
