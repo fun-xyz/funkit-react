56a824d3a53e5bf9642f5563d63f16ae444abe38
aaronchewbani <aaronchewbani@gmail.com>
2023-07-05 18:28:02 -0700

    cleanup

--- src/hooks/UseGetAssets.ts
+++ src/hooks/UseGetAssets.ts
@@ -4,22 +4,22 @@ import { useFunStoreInterface } from '../store/CreateUseFunStore'
 import { useFun } from './UseFun'
 
 export const useGetAssets = (build: any) => {
-  const { FunWallet, index, error, setTempError, setAssets, assets } = useFun((state: useFunStoreInterface) => ({
+  const { FunWallet, setAssets, assets } = useFun((state: useFunStoreInterface) => ({
     FunWallet: state.FunWallet,
-    index: state.index,
-    error: state.error,
-    setIndex: state.setIndex,
-    setTempError: state.setTempError,
     setAssets: state.setAssets,
     assets: state.assets,
   }))
 
   const getAssets = useCallback(async () => {
     if (FunWallet) {
-      const getAssetResult = await FunWallet.getAssets()
-      setAssets(getAssetResult)
+      try {
+        const getAssetResult = await FunWallet.getAssets()
+        setAssets(getAssetResult)
+      } catch (e) {
+        console.log(e, 'Failed to get Assets')
+      }
     }
   }, [assets, FunWallet])
 
-  return { index, error, setTempError, getAssets }
+  return { getAssets }
 }
--- src/store/plugins/AssetsStore.ts
+++ src/store/plugins/AssetsStore.ts
@@ -1,18 +1,3 @@
-import { GlobalEnvOption } from '@fun-xyz/core'
-
-import { convertToChain } from '../../network/Networks'
-import { buildAndUpdateConfig } from './ConfigureStore'
-
 export interface AssetStoreInterface {
   asset: object | null
-}
-
-export const handleChainSwitching = async (newChain: number | string, oldConfig: Partial<GlobalEnvOption> | null) => {
-  const Chain = convertToChain(newChain)
-  if (!Chain) {
-    throw new Error(`Invalid Chain: ${newChain}`)
-  }
-  const { config } = await buildAndUpdateConfig({ chain: Chain }, { ...oldConfig })
-
-  return { chain: Chain, chainId: Chain.chainId, config }
 }
