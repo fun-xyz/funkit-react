457c81ef0736e853e9b6238e68c6b064a0fe81bd
Nighttrek <Daniels@dual4t.com>
2023-08-10 18:30:56 -0700

    updated for tree shaking

--- src/hooks/actions/index.ts
+++ src/hooks/actions/index.ts
@@ -1,5 +1,22 @@
-export * from './types'
-export * from './UseActions'
-export * from './UseGroup'
-export * from './UseOperations'
+export {
+  ActionType,
+  FirstClassActionParams,
+  IAddOwnerParams,
+  IAddUserToGroupParams,
+  IApprove,
+  ICreateGroupParams,
+  ICreateSessionKeyParams,
+  IExecRawTxParams,
+  IRemoveGroupParams,
+  IRemoveOwnerParams,
+  IRemoveUserFromGroupParams,
+  IStakeParams,
+  ISwapParams,
+  ITransfer,
+  IUnstakeParams,
+  IUpdateThresholdOfGroupParams,
+} from './types'
+export { useAction } from './UseActions'
+export { useGroup } from './UseGroup'
+export { IUseOperationReturn, useOperations } from './UseOperations'
 export { useRBAC } from './UseRBAC'
