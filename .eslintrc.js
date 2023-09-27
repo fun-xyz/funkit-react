8e8c7916cab84995cece137b51d89507eaed1af2
Nighttrek <Daniels@dual4t.com>
2023-09-26 17:15:02 -0700

    updated the lint  rules to exclude floating promises

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,15 @@ require('@uniswap/eslint-config/load')
 
 module.exports = {
   extends: '@uniswap/eslint-config/react',
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json', // Path to your TypeScript configuration file
+    ecmaVersion: 2021, // Or whichever version you are using
+    sourceType: 'module',
+  },
   rules: {
     'import/no-unused-modules': 'off',
+    "@typescript-eslint/no-floating-promises": "error"
   },
+  ignorePatterns: ['**/.eslintrc.js'], // Exclude ESLint configuration file
 }
