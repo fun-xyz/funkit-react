b3bf93a60bfb0907634217e8d1688ccab8f2aa7f
Nighttrek <Daniels@dual4t.com>
2023-10-10 00:19:36 -0700

    Manually added the connector class and modified it to support updates and better initialization tracking

--- src/connectors/MetaMask.ts
+++ src/connectors/MetaMask.ts
@@ -1,4 +1,5 @@
 import { initializeConnector } from '@web3-react/core'
 import { MetaMask } from '@web3-react/metamask'
 
-export const [connector, hooks] = initializeConnector<MetaMask>((actions) => new MetaMask({ actions }))
+export const InjectedConnector = initializeConnector<MetaMask>((actions) => new MetaMask({ actions }))
+export const [connector, hooks, store] = InjectedConnector
