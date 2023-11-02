d4344a4d52d1a98a02d1251890a95979d3c8b6d2
Albert Su <albertsu2001@gmail.com>
2023-11-02 15:19:15 -0700

    ✅Use endpoint for createPrivateKey

--- src/hooks/auth/UseTurnkeyAuth.tsx
+++ src/hooks/auth/UseTurnkeyAuth.tsx
@@ -1,9 +1,7 @@
-import { Auth, createSubOrg } from '@funkit/core'
-import { ApiKeyStamper } from '@turnkey/api-key-stamper'
-import { createActivityPoller, getWebAuthnAttestation, TurnkeyClient } from '@turnkey/http'
+import { Auth, createPrivateKey, createSubOrg } from '@funkit/core'
+import { getWebAuthnAttestation, TurnkeyClient } from '@turnkey/http'
 import { createAccount } from '@turnkey/viem'
 import { WebauthnStamper } from '@turnkey/webauthn-stamper'
-import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { createWalletClient, http, WalletClient } from 'viem'
 import { goerli } from 'viem/chains'
@@ -54,7 +52,7 @@ export const useTurnkeyAuth = (readonly = false): authHookReturn => {
     stamper
   )
 
-  const createPrivateKey = async (subOrgId: string): Promise<any> => {
+  const createPrivateKeyReact = async (subOrgId: string): Promise<any> => {
     if (!subOrgId) {
       throw new Error('sub-org id not found')
     }
@@ -74,51 +72,7 @@ export const useTurnkeyAuth = (readonly = false): authHookReturn => {
         ],
       },
     })
-
-    const activityResponse = await axios.post(signedRequest.url, signedRequest.body, {
-      headers: {
-        [signedRequest.stamp.stampHeaderName]: signedRequest.stamp.stampHeaderValue,
-      },
-    })
-
-    if (activityResponse.status !== 200) {
-      throw new Error('Failed to get response')
-    }
-
-    const stamper = new ApiKeyStamper({
-      apiPublicKey: '0306bbf329c279010ddc2ad278cb05f64b9bb53cd869f5bce3b3e93d440b26166a',
-      apiPrivateKey: '43547e88f7a8b10adf81bf02071f4cfa89bfa5ad6c773f006301d8ee3a40ceb9',
-    })
-    const client = new TurnkeyClient({ baseUrl: 'https://api.turnkey.com' }, stamper)
-
-    const activityPoller = createActivityPoller({
-      client,
-      requestFn: client.getActivity,
-    })
-
-    const activityId = refineNonNull(activityResponse.data.activity?.id)
-    const newSubOrgId = refineNonNull(activityResponse.data.activity?.organizationId)
-
-    const completedActivity = await activityPoller({
-      activityId,
-      organizationId: newSubOrgId,
-    })
-
-    const privateKeys = completedActivity.result.createPrivateKeysResultV2?.privateKeys
-
-    // XXX: sorry for the ugly code! We expect a single key / address returned.
-    // If we have more than one key / address returned, or none, this would break.
-    const address = privateKeys?.map((pk) => pk.addresses?.map((addr) => addr.address).join('')).join('')
-    const privateKeyId = privateKeys?.map((pk) => pk.privateKeyId).join('')
-
-    setPrivateKey({
-      id: privateKeyId!,
-      address: address!,
-    })
-    return {
-      id: privateKeyId!,
-      address: address!,
-    }
+    return await createPrivateKey(signedRequest)
   }
 
   const createAuth = async (subOrgId: string, privateKey: any) => {
@@ -185,7 +139,7 @@ export const useTurnkeyAuth = (readonly = false): authHookReturn => {
   // Should create a subOrg all the way to an auth
   const doEverything = async () => {
     const newSubOrgId = await createSubOrganization()
-    const newPrivateKey = await createPrivateKey(newSubOrgId)
+    const newPrivateKey = await createPrivateKeyReact(newSubOrgId)
     await createAuth(newSubOrgId, newPrivateKey)
   }
 
