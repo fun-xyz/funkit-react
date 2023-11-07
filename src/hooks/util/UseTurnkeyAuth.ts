104f4d1214ab7b2159c8e545f0241f4451b66a6e
Albert Su <albertsu2001@gmail.com>
2023-11-06 22:32:24 -0500

    Turnkey + Bump node
    Turnkey (#32)
    
    * ✨Working yarn.lock
    
    * ✨Add scaffolding to useTurnkeyAuth
    
    * ✨Add imports to package.json, remove unused imports, rename functions
    
    * ✨Start debugging
    
    * ✨Attempt to cast the type
    
    * ✅Fix casting issue
    
    * ✅Export useTurnkeyAuth
    
    * ✨Hardcode base url
    
    * ✨Working turnkey
    
    * 💩temp stash
    
    * ✨Working createSubOrg
    
    * ✅Working createPrivateKEy
    
    * ✅Remove console.logs and unused apis
    
    * ✅Remove yarn-error.log and add to .gitignore
    
    * ✅Use endpoint for createPrivateKey
    
    * ✅Remove useEffect and add logout
    
    * ✨Let users pass in an rpId
    
    * ✅Address comments
    
    * ✨Add defaultSubOrg
    
    * ✅Fully functional useTurnkeyHook
    
    * ✅Fully working with passkey reuse
    
    * 💅Remove console.logs
    
    * 💅Remove unused npm packages
    
    * ✅Address all comments
    
    * ✅Fix comments, move constants up to top of file and memoization go brrrrr🚒
    
    * ✅Extend authHookReturn instead of creating a new type
    
    * ✅Address comments, move functions and constants to new folders

---
+++ src/hooks/util/UseTurnkeyAuth.ts
@@ -0,0 +1,13 @@
+export const generateRandomBuffer = (): ArrayBuffer => {
+  const arr = new Uint8Array(32)
+  crypto.getRandomValues(arr)
+  return arr.buffer
+}
+
+export const base64UrlEncode = (challenge: ArrayBuffer): string => {
+  return Buffer.from(challenge).toString('base64').replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, '')
+}
+
+export const humanReadableDateTime = (): string => {
+  return new Date().toLocaleString().replace(/\//g, '-').replace(/:/g, '.')
+}
